f59714eee0ec96b855ee45be57257d20e5917e84
Bertold Kolics <bkolics@verosint.com>
2024-06-30 18:10:29 -0500

    PR feedback

--- zip_test.go
+++ zip_test.go
@@ -1,6 +1,7 @@
-package xtractr
+package xtractr_test
 
 import (
+	"archive/zip"
 	_ "embed"
 	"os"
 	"path/filepath"
@@ -8,27 +9,51 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"golift.io/xtractr"
 )
-
-//go:embed test_data/archive.zip
-var zipData []byte
 
 func TestZip(t *testing.T) {
 	t.Parallel()
 	const (
-		testDataSize  = 35
-		testFileCount = 3
+		testDataSize     = 21
+		testFileCount    = 5
+		testArchiveCount = 1
 	)
+	testFiles := []string{
+		"README.txt",
+		"subdir/",
+		"subdir/subdirfile.txt",
+		"subdir/level2/",
+		"subdir/level2/level2file.txt",
+	}
 
 	name, err := os.MkdirTemp(".", "xtractr_test_*_data")
 	require.NoError(t, err, "creating temp directory failed")
 	defer os.RemoveAll(name)
 
-	zipTestFile := filepath.Join(name, "archive.zip")
-	err = os.WriteFile(zipTestFile, zipData, 0o600)
+	zipFile, err := os.Create(filepath.Join(name, "archive.zip"))
+	require.NoError(t, err)
+	zipWriter := zip.NewWriter(zipFile)
+
+	for _, file := range testFiles {
+		if file[len(file)-1] == '/' {
+			_, err = zipWriter.Create(file)
+			require.NoError(t, err)
+		} else {
+			f, err := zipWriter.Create(file)
+			require.NoError(t, err)
+			_, err = f.Write([]byte("content"))
+			require.NoError(t, err)
+		}
+	}
+	err = zipWriter.Close()
+	require.NoError(t, err)
+	err = zipFile.Close()
 	require.NoError(t, err)
 
-	size, files, err := ExtractZIP(&XFile{
+	zipTestFile := filepath.Join(name, "archive.zip")
+
+	size, files, archives, err := xtractr.ExtractFile(&xtractr.XFile{
 		FilePath:  zipTestFile,
 		OutputDir: filepath.Clean(name),
 		FileMode:  0o600,
@@ -37,4 +62,5 @@ func TestZip(t *testing.T) {
 	require.NoError(t, err)
 	assert.Equal(t, int64(testDataSize), size)
 	assert.Len(t, files, testFileCount)
+	assert.Len(t, archives, testArchiveCount)
 }
